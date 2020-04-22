5eade8c5c865da7e46fc4391b0a98fd8f0ed34cd
Konstantin Samarin <samarink@protonmail.com>
2020-04-22 22:42:15 +0700

    refactor params to it's own method

--- rails/routes-and-controllers/app/controllers/users_controller.rb
+++ rails/routes-and-controllers/app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params.require(:user).permit(:name, :email))
+    @user = User.new(user_params)
 
     if @user.save
       render json: @user
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[id])
 
-    if @user.update(params.require(:user).permit(:name, :email))
+    if @user.update(user_params)
       render json: @user
     else
       render json: @user.errors.full_messages, status: :unprocessable_entity
@@ -33,5 +33,11 @@ class UsersController < ApplicationController
     @user = User.find(params[id])
     @user.destroy
     render json: @user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
