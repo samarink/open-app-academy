a031206642feea8cfff7afcfa2c3e8d24425c2b0
Konstantin Samarin <samarink@protonmail.com>
2020-04-22 22:28:15 +0700

    add error handling

--- rails/routes-and-controllers/app/controllers/users_controller.rb
+++ rails/routes-and-controllers/app/controllers/users_controller.rb
@@ -6,9 +6,12 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(params.require(:user).permit(:name, :email))
-    user.save!
 
-    render json: user
+    if user.save
+      render json: user
+    else
+      render json: user.errors.full_messages, status: :unprocessable_entity
+    end
   end
 
   def show
