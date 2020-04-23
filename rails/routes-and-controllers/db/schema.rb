21f77e22dbf69c2d7454f34f5130e991f4947689
Konstantin Samarin <samarink@protonmail.com>
2020-04-23 18:30:17 +0700

    drop email, rename name to username

--- rails/routes-and-controllers/db/schema.rb
+++ rails/routes-and-controllers/db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_22_145942) do
+ActiveRecord::Schema.define(version: 2020_04_23_112741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "email", null: false
+    t.string "username", null: false
   end
 
 end
