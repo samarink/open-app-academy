440f3077c4c3c48acf034ebcc4a0c9bcab7fc309
Konstantin Samarin <samarink@protonmail.com>
2020-04-23 21:16:58 +0700

    fix typo

--- rails/routes-and-controllers/app/models/user.rb
+++ rails/routes-and-controllers/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniquiness: true
+  validates :username, presence: true, uniqueness: true
 
   has_many :artworks, foreign_key: :artist_id, dependent: :destroy
   has_many :artwork_shares, foreign_key: :viewer_id, dependent: :destroy
