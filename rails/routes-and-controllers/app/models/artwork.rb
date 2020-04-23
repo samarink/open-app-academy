dddd772c9ea3102d18be00ebbcbf7efa25e004b9
Konstantin Samarin <samarink@protonmail.com>
2020-04-23 20:18:19 +0700

    add associations

--- rails/routes-and-controllers/app/models/artwork.rb
+++ rails/routes-and-controllers/app/models/artwork.rb
@@ -3,5 +3,7 @@ class Artwork < ApplicationRecord
   validates :image_url, uniquiness: true
   validates :title, uniquiness: { scope: :artist_id }
 
+  has_many :artwork_shares
+  has_many :shared_viewers, through: :artwork_shares, source: :viewer
   belongs_to :artist, foreign_key: :artist_id, class_name: 'User'
 end
