77e02dfcaa80b889cf4729c2cc9d0e55ed7985b8
Konstantin Samarin <samarink@protonmail.com>
2020-04-24 19:21:10 +0700

    add search functionality

--- rails/routes-and-controllers/app/controllers/users_controller.rb
+++ rails/routes-and-controllers/app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    if params[:query]
+      @users = User.where('username LIKE ?', "%#{params[:query]}%")
+    else
+      @users = User.all
+    end
+
     render json: @users
   end
 
