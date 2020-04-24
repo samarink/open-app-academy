5260579b7e8e366aea8d4967be9ab4b67c7a07f1
Konstantin Samarin <samarink@protonmail.com>
2020-04-24 16:03:11 +0700

    add comments association

--- rails/routes-and-controllers/app/models/artwork.rb
+++ rails/routes-and-controllers/app/models/artwork.rb
@@ -5,6 +5,7 @@ class Artwork < ApplicationRecord
 
   has_many :artwork_shares
   has_many :shared_viewers, through: :artwork_shares, source: :viewer
+  has_many :comments, dependent: :destroy
   belongs_to :artist, foreign_key: :artist_id, class_name: 'User'
 
   def self.artworks_for_user_id(user_id)
--- rails/routes-and-controllers/app/models/user.rb
+++ rails/routes-and-controllers/app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   has_many :artworks, foreign_key: :artist_id, dependent: :destroy
   has_many :artwork_shares, foreign_key: :viewer_id, dependent: :destroy
   has_many :shared_artworks, through: :artwork_shares, source: :artwork
+  has_many :comments, dependent: :destroy
 end
