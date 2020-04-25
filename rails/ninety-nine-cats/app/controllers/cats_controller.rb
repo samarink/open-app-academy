8a82d1e4a35c23b17898753d9c4c7f2d9e9a5304
Konstantin Samarin <samarink@protonmail.com>
2020-04-25 22:25:34 +0700

    create cats controller

---
+++ rails/ninety-nine-cats/app/controllers/cats_controller.rb
@@ -0,0 +1,11 @@
+class CatController < ApplicationController
+  def index
+    @cats = Cat.all
+    render :index
+  end
+
+  def show
+    @cat = Cat.find_by(id: params[:id])
+    render :show
+  end
+end
