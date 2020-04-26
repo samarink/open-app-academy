67ba8a6df86f6a97837976008b3186db218e7389
Konstantin Samarin <samarink@protonmail.com>
2020-04-26 22:10:23 +0700

    add new and edit forms and according controller action

--- rails/ninety-nine-cats/app/controllers/cats_controller.rb
+++ rails/ninety-nine-cats/app/controllers/cats_controller.rb
@@ -8,4 +8,42 @@ class CatController < ApplicationController
     @cat = Cat.find_by(id: params[:id])
     render :show
   end
+
+  def new
+    @cat = Cat.new
+    render :new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      flash.now[:errors] = @cat.errors.full_messages
+      render :new
+    end
+  end
+
+  def edit
+    @cat = Cat.find_by(id: params[:id])
+    render :edit
+  end
+
+  def update
+    @cat = Cat.find_by(id: params[:id])
+
+    if @cat.update_attributes(cat_params)
+      redirect_to cat_url(@cat)
+    else
+      flash.now[:errors] = @cat.errors.full_messages
+      render :edit
+    end
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:age, :birth_date, :color, :description)
+  end
 end
