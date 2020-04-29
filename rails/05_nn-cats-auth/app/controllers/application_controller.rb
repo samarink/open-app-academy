7103458647ddf592b6766f96339228955c0bfc82
Konstantin Samarin <samarink@protonmail.com>
2020-04-29 18:57:52 +0700

    add current_user and logout_user methods

--- rails/05_nn-cats-auth/app/controllers/application_controller.rb
+++ rails/05_nn-cats-auth/app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    return nil unless session[:session_token]
+    @user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def logout_user!
+    if current_user
+      current_user.reset_session_token!
+      session[session_token] = nil
+    end
+  end
 end
