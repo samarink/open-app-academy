1eb8c9ece117b4acae5c5c8ec6e6ca7ac78310f3
Konstantin Samarin <samarink@protonmail.com>
2020-04-29 18:50:19 +0700

    create session controller

---
+++ rails/05_nn-cats-auth/app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password],
+    )
+
+    if user.nil?
+      flash.now[:errors] = ['Incorrect username/password']
+      render :new
+    else
+      login_user!(user)
+      redirect_to cats_url
+    end
+  end
+end
