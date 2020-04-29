7c4f34e039be9ad6fa6d6b14c8690a55192aad33
Konstantin Samarin <samarink@protonmail.com>
2020-04-29 20:57:43 +0700

    add helpers

--- rails/05_nn-cats-auth/app/controllers/application_controller.rb
+++ rails/05_nn-cats-auth/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  helper_method :logged_in?
 
   def current_user
     return nil unless session[:session_token]
@@ -15,5 +16,17 @@ class ApplicationController < ActionController::Base
 
   def require_no_user
     redirect_to cats_url if current_user
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def login_user!(user)
+    session[session_token] = user.reset_session_token!
+  end
+
+  def require_user
+    redirect_to new_session_url if current_user.nil?
   end
 end
