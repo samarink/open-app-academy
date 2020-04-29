b9f144acc70b555a1265ea0b5801232ad5505455
Konstantin Samarin <samarink@protonmail.com>
2020-04-29 17:12:51 +0700

    add validations

--- rails/rails_auth_hw/app/models/user.rb
+++ rails/rails_auth_hw/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :username, :session_token, presence: true
+  validates :password_digest, presence: { message: 'Password can\'t be blank' }
+  validates :password, length: { minimun: 6, allow_nil: true }
+  before_validation :ensure_session_token
 end
