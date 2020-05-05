53616a3d5191adf8ec8de8facc4de79f5f43e8a3
Konstantin Samarin <samarink@protonmail.com>
2020-05-05 18:58:14 +0700

    add controller testing

--- 04_rails/07_music_app/spec/controllers/users_controller_spec.rb
+++ 04_rails/07_music_app/spec/controllers/users_controller_spec.rb
@@ -1,25 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+
+  describe "GET #new" do
+    it "renders the new template" do
+      get :new, {}
+      expect(response).to render_template("new")
+    end
+  end
+
   describe "POST #create" do
     context "with invalid params" do
       it "validates the presence of the user's email and password" do
-        post :create, params: { user: { email: "jack_bruce@place.com", password: "" } }
+        post :create, params: { user: {email: "email@ex.com", password: ""} }
         expect(response).to render_template("new")
-        expect(flash[:errors]).to be_present
+        expect(flash.now[:errors]).to be_present
       end
 
       it "validates that the password is at least 6 characters long" do
-        post :create, params: { user: { email: "jack_bruce@place.com", password: "short" } }
+        post :create, params: { user: {email: "email@ex.com", password: "short"} }
         expect(response).to render_template("new")
-        expect(flash[:errors]).to be_present
+        expect(flash.now[:errors]).to be_present
       end
     end
 
     context "with valid params" do
-      it "redirects user to sign-in page on success" do
-        post :create, params: { user: { email: "jack_bruce@place.com", password: "password" } }
-        expect(response).to redirect_to(new_session_url)
+      it "redirects user to bands index on success" do
+        post :create, params: { user: { email: "testing@email.com", password: "biscuits" } }
+        expect(response).to redirect_to(root_url)
       end
     end
   end
