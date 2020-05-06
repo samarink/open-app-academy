9225073fe9db03ba23ba610f03fecdc54fea9178
Konstantin Samarin <samarink@protonmail.com>
2020-05-06 14:46:18 +0700

    ditching

---
+++ 04_rails/08_goals_app/spec/controllers/users_controller_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+require 'byebug'
+
+RSpec.describe UsersController, type: :controller do
+
+  subject(:user) do
+    User.create(
+      username: "testing@mail.com",
+      password: "biscuits"
+    )
+  end
+
+  describe "GET #new" do
+    it "renders the new template" do
+      get :new, {}
+      expect(response).to render_template("new")
+    end
+  end
+
+  describe "POST #create" do
+    context "with invalid params" do
+      it "validates the presence of the user's email and password" do
+        post :create, params: { user: {email: "email@ex.com", password: ""} }
+        expect(response).to render_template("new")
+        expect(flash.now[:errors]).to be_present
+      end
+
+      it "validates that the password is at least 6 characters long" do
+        post :create, params: { user: {email: "email@ex.com", password: "short"} }
+        expect(response).to render_template("new")
+        expect(flash.now[:errors]).to be_present
+      end
+    end
+
+    context "with valid params" do
+      it "redirects user to bands index on success" do
+        post :create, params: { user: { email: "testing@email.com", password: "biscuits" } }
+        expect(response).to redirect_to(root_url)
+      end
+    end
+  end
+end
+
