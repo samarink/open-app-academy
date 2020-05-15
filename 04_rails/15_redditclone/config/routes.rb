caf58b314303454e0b9fea3ef081ec4fb9c4442f
Konstantin Samarin <samarink@protonmail.com>
2020-05-15 16:43:43 +0700

    add routes for users and subs

--- 04_rails/15_redditclone/config/routes.rb
+++ 04_rails/15_redditclone/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users
+  resources :users, only: [:new, :create]
+  resources :subs, except: [:destroy]
   resource :session, only: [:create, :new, :destroy]
 end
