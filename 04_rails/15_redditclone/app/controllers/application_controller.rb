4d80ea79f2ce4dc9a3d192e9c1afcce02f1d3835
Konstantin Samarin <samarink@protonmail.com>
2020-05-15 23:04:07 +0700

    add more helpers to app controller

--- 04_rails/15_redditclone/app/controllers/application_controller.rb
+++ 04_rails/15_redditclone/app/controllers/application_controller.rb
@@ -1,8 +1,15 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
 
+  private
   def login!(user)
     @current_user = user
     session_token[:session_token] = user.session_token
+  end
+
+  def logged_in?
+    !!current_user
   end
 
   def current_user
@@ -13,5 +20,13 @@ class ApplicationController < ActionController::Base
   def logout!
     current_user.try(:reset_session_token!)
     session[:session_token] = nil
+  end
+
+  def require_logged_in!
+    redirect_to new_session_url unless logged_in?
+  end
+
+  def require_logged_out!
+    redirect_to user_url(current_user) if logged_in?
   end
 end
