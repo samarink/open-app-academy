d11a0f3a47d7167a676b09d9dc07e890fc7fb889
Konstantin Samarin <konstsamari@gmail.com>
2020-05-19 17:45:45 +0700

    add helpers to controller base

--- 04_rails/17_rails_lite/lib/controller_base.rb
+++ 04_rails/17_rails_lite/lib/controller_base.rb
@@ -8,20 +8,33 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
+    @req, @res = req, res
+    @already_built_response = false
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
   def redirect_to(url)
+    raise 'Double redirect' if already_built_response?
+
+    res.status = 302
+    res.location = url
+    @already_built_response = true
   end
 
   # Populate the response with content.
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise 'Double render' if already_built_response?
+
+    @res.content_type = content_type
+    @res.write(content)
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
