38b0c4bd1b7750fd67fca6a17bf7545cbc3bba94
Konstantin Samarin <konstsamari@gmail.com>
2020-05-19 19:51:53 +0700

    add router methods

--- 04_rails/17_rails_lite/lib/router.rb
+++ 04_rails/17_rails_lite/lib/router.rb
@@ -2,10 +2,15 @@ class Route
   attr_reader :pattern, :http_method, :controller_class, :action_name
 
   def initialize(pattern, http_method, controller_class, action_name)
+    @pattern = pattern
+    @http_method = http_method
+    @controller_class = controller_class
+    @action_name = action_name
   end
 
   # checks if pattern matches path and method matches request method
   def matches?(req)
+    !!(req.path =~ @pattern) && (req.method.downcase.to_sym == @http_method)
   end
 
   # use pattern to pull out route params (save for later?)
@@ -18,10 +23,17 @@ class Router
   attr_reader :routes
 
   def initialize
+    @routes = []
   end
 
   # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
+    @routes << Route.new(
+      pattern,
+      method,
+      controller_class,
+      action_name
+    )
   end
 
   # evaluate the proc in the context of the instance
@@ -32,10 +44,14 @@ class Router
   # make each of these methods that
   # when called add route
   [:get, :post, :put, :delete].each do |http_method|
+    define_method(http_method) do |pattern, controller_class, action_name|
+      add_route(pattern, http_method, controller_class, action_name)
+    end
   end
 
   # should return the route that matches this request
   def match(req)
+    @routes.find { |route| route.matches?(req) }
   end
 
   # either throw 404 or call run on a matched route
