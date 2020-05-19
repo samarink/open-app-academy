51797f7e2d536652a4e41caa78d8c24e224b816e
Konstantin Samarin <konstsamari@gmail.com>
2020-05-19 16:58:09 +0700

    add basic app and server

--- 04_rails/17_rails_lite/bin/p01_basic_server.rb
+++ 04_rails/17_rails_lite/bin/p01_basic_server.rb
@@ -1,1 +1,17 @@
 require 'rack'
+
+app = Proc.new do |env|
+  req = Rack::Request.new(env)
+  res = Rack::Response.new
+
+  path = req.path
+
+  res['Content-Type'] = 'text/html'
+  res.write(path)
+  res.finish
+end
+
+Rack::Server.start(
+  app: app,
+  Port: 3000
+)
