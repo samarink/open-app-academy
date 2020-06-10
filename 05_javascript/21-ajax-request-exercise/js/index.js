f8da5e840fb93602395185cb9f03ba3bc423166c
Konstantin Samarin <konstsamari@gmail.com>
2020-06-10 22:05:42 +0700

    jquery ajax: complete

---
+++ 05_javascript/21-ajax-request-exercise/js/index.js
@@ -0,0 +1,16 @@
+console.log("Hello from the JavaScript console!");
+
+// Your AJAX request here
+$.ajax({
+  type: 'GET',
+  url: 'http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b',
+  success(res) {
+    console.log(res);
+  },
+  error() {
+    console.log('An error occured');
+  }
+})
+
+// Add another console log here, outside your AJAX request
+console.log('Outside of ajax request');
