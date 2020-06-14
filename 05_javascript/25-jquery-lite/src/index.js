239414f09d6bccbe54d7825192be746db22a2a1e
Konstantin Samarin <konstsamari@gmail.com>
2020-06-14 21:27:32 +0700

    jquery lite: add ajax method

--- 05_javascript/25-jquery-lite/src/index.js
+++ 05_javascript/25-jquery-lite/src/index.js
@@ -16,7 +16,57 @@ window.$l = (arg) => {
   }
 }
 
+$l.extend = (base, ...otherObjs) => {
+  otherObjs.forEach(obj => {
+    for (const prop in obj) {
+      base[prop] = obj[prop];
+    }
+  });
+
+  return base;
+}
+
+$l.ajax = (options) => {
+  const request = new XMLHttpRequest();
+  const defaults = {
+    contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+    method: "GET",
+    url: "",
+    success: () => {},
+    error: () => {},
+    data: {},
+  };
+  options = $l.extend(defaults, options);
+  options.method = options.method.toUpperCase();
+
+  if (options.method === "GET") {
+    options.url += `?${toQueryString(options.data)}`;
+  }
+
+  request.open(options.method, options.url, true);
+  request.onload = (e) => {
+    if (request.status === 200) {
+      options.success(request.response);
+    } else {
+      options.error(request.response);
+    }
+  }
+
+  request.send(JSON.stringify(options.data));
+}
+
 // helpers
+
+toQueryString = (obj) => {
+  let result = "";
+  for (const prop in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+      result += `${prop}=${obj[prop]}&`;
+    }
+  }
+  return result.substring(0, result.length - 1);
+};
+
 getNodesFromDom = (selector) => {
   const nodeList = document.querySelectorAll(selector);
   const nodeListArr = Array.from(nodeList);
