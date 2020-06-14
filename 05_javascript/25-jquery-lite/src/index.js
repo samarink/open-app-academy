1f02d2596bb78851c4c9652b70fc801e88316982
Konstantin Samarin <konstsamari@gmail.com>
2020-06-14 21:08:08 +0700

    jquery lite: accept function as document onload parametr

--- 05_javascript/25-jquery-lite/src/index.js
+++ 05_javascript/25-jquery-lite/src/index.js
@@ -1,15 +1,37 @@
 const DomNodeCollection = require('./dom_node_collection');
 
-function $l(arg) {
+const _docReadyCallbacks = [];
+let _docReady = false;
 
-  if (typeof arg === 'string') {
-    const nodeList = document.querySelectorAll(arg);
-    const nodeListArr = Array.from(nodeList);
-    return new DomNodeCollection(nodeListArr);
-
-  } else if (arg instanceof HTMLElement) {
-    return new DomNodeCollection(arg);
+window.$l = (arg) => {
+  switch (typeof arg) {
+    case 'function':
+      return registerDocReadyCallback(arg);
+    case 'string':
+      return getNodesFromDom(arg);
+    case 'object':
+      if (arg instanceof HTMLElement) {
+        return new DomNodeCollection([arg]);
+      }
   }
 }
 
-window.$l = $l;
+// helpers
+getNodesFromDom = (selector) => {
+  const nodeList = document.querySelectorAll(selector);
+  const nodeListArr = Array.from(nodeList);
+  return new DomNodeCollection(nodeListArr);
+}
+
+registerDocReadyCallback = (func) => {
+  if (!_docReady) {
+    _docReadyCallbacks.push(func);
+  } else {
+    func();
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  _docReady = true;
+  _docReadyCallbacks.forEach(func => func());
+});
