c4859142d3302330efc1c3459281142dee8c8f2b
Konstantin Samarin <konstsamari@gmail.com>
2020-06-17 02:20:08 +0700

    redux lite: subscribe callbacks

--- 06_react/01-redux-lite/redux.js
+++ 06_react/01-redux-lite/redux.js
@@ -2,6 +2,7 @@ class Store {
   constructor(rootReducer) {
     this.rootReducer = rootReducer;
     this.state = {};
+    this.subscriptions = [];
 
     this.getState = this.getState.bind(this);
   }
@@ -11,24 +12,36 @@ class Store {
   }
 
   dispatch(action) {
-    this.state = this.rootReducer(this.state, action);
+    this.state = this.rootReducer(this.state, action, this.subscriptions);
+  }
+
+  subscribe(cb) {
+    this.subscriptions.push(cb);
   }
 }
 
 const createStore = (...args) => new Store(...args);
 
 const combineReducers = config => {
-  return (prevState, action) => {
+  return (prevState, action, subscriptions) => {
     const nextState = {};
+    let stateChanged = false;
     Object.keys(config).forEach(k => {
       if (!action) {
         const args = [ , { type: "__initialize" }];
         nextState[k] = config[k](...args);
+        stateChanged = true;
       } else {
-        nextState[k] = config[k](prevState[k], action);
+        const next = config[k](prevState[k], action);
+        if (next !== prevState[k]) stateChanged = true;
+        nextState[k] = next;
       }
     });
 
-    return nextState;
+    if (stateChanged) {
+      if (subscriptions) subscriptions.forEach(cb => cb(nextState));
+      return nextState;
+    }
+    return prevState;
   }
 }
