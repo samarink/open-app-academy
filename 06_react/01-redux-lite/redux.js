6ab2715ea9f721050db93300c679a81064abf5f8
Konstantin Samarin <konstsamari@gmail.com>
2020-06-16 23:38:03 +0700

    redux lite: combine reducers function

--- 06_react/01-redux-lite/redux.js
+++ 06_react/01-redux-lite/redux.js
@@ -10,3 +10,16 @@ class Store {
     return Object.assign(this.state); // shallow copy
   }
 }
+
+const createStore = (...args) => new Store(...args);
+
+const combineReducers = config => {
+  return (prevState, action) => {
+    const nextState = {};
+    Object.keys(prevState).forEach(k => {
+      nextState[k] = config[k](prevState[k], action);
+    });
+
+    return nextState;
+  }
+}
