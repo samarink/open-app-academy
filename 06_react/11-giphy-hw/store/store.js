4a28debc94c5a81189ef1e9feadebc606647eb0c
Konstantin Samarin <konstsamari@gmail.com>
2020-07-05 20:13:16 +0700

    giphy: store

--- 06_react/11-giphy-hw/store/store.js
+++ 06_react/11-giphy-hw/store/store.js
@@ -2,4 +2,8 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
 
+const configureStore = () => (
+  createStore(rootReducer)
+)
 
+export default configureStore;
