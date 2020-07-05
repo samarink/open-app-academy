a5ebd35fa67045c7560fd3f08697f5136f016cc6
Konstantin Samarin <konstsamari@gmail.com>
2020-07-05 23:12:56 +0700

    giphy: presentational component

--- 06_react/11-giphy-hw/reducers/giphys_reducer.js
+++ 06_react/11-giphy-hw/reducers/giphys_reducer.js
@@ -1,6 +1,6 @@
 import { RECEIVE_SEARCH_GIPHYS } from '../actions/giphy_actions';
 
-const giphysReducer = (state = {}, action) => {
+const giphysReducer = (state = [], action) => {
   switch(action.type) {
     case RECEIVE_SEARCH_GIPHYS:
       return action.giphys;
