1ef788a855867d2e6c10245e16ad9b2d437af070
Konstantin Samarin <konstsamari@gmail.com>
2020-07-06 21:45:37 +0700

    pokedex p1: selectAllPokemon selector

--- 06_react/13-pokedex-p1/frontend/pokedex.jsx
+++ 06_react/13-pokedex-p1/frontend/pokedex.jsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 
 // test
-import { receiveAllPokemon } from './actions/pokemon_actions';
+import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
 import { fetchAllPokemon } from './util/api_util';
+import { selectAllPokemon } from './reducers/selectors';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store = configureStore();
@@ -14,6 +15,8 @@ document.addEventListener('DOMContentLoaded', () => {
   window.dispatch=store.dispatch;
   window.receiveAllPokemon=receiveAllPokemon;
   window.fetchAllPokemon=fetchAllPokemon;
+  window.selectAllPokemon=selectAllPokemon;
+  window.requestAllPokemon=requestAllPokemon;
 
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Pokedex</h1>, root);
