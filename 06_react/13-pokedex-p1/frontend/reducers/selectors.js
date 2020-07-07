6a916f33e32a9a7c25052219ea9603c760e131fa
Konstantin Samarin <konstsamari@gmail.com>
2020-07-07 21:47:38 +0700

    pokedex p2: select poke items selector

--- 06_react/13-pokedex-p1/frontend/reducers/selectors.js
+++ 06_react/13-pokedex-p1/frontend/reducers/selectors.js
@@ -1,1 +1,5 @@
 export const selectAllPokemon = state => Object.values(state.entities.pokemon);
+
+export const selectPokeItems = (state, poke) => {
+  return poke ? poke.item_ids.map(id => state.entities.items[id]) : [];
+};
