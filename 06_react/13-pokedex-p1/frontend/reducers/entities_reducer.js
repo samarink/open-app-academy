bba936fe5ae5ebf4c7e7e11ceae24efd67cfde31
Konstantin Samarin <konstsamari@gmail.com>
2020-07-07 21:42:34 +0700

    pokedex p2: update entities reducer

--- 06_react/13-pokedex-p1/frontend/reducers/entities_reducer.js
+++ 06_react/13-pokedex-p1/frontend/reducers/entities_reducer.js
@@ -1,8 +1,11 @@
-import pokemonReducer from './pokemon_reducer';
 import { combineReducers } from 'redux';
 
+import pokemonReducer from './pokemon_reducer';
+import itemsReducer from './items_reducer';
+
 const entitiesReducer = combineReducers({
-  pokemon: pokemonReducer
+  pokemon: pokemonReducer,
+  items: itemsReducer
 });
 
 export default entitiesReducer;
