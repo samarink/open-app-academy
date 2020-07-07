b23c1b47db8517980694c0fcbac0a30c1d46e6d7
Konstantin Samarin <konstsamari@gmail.com>
2020-07-07 21:28:19 +0700

    pokedex p2: receive single pokemon actions

--- 06_react/13-pokedex-p1/frontend/actions/pokemon_actions.js
+++ 06_react/13-pokedex-p1/frontend/actions/pokemon_actions.js
@@ -1,9 +1,15 @@
 import * as PokemonAPIUtil from '../util/api_util';
 
 export const RECEIVE_ALL_POKEMON = 'RECEIVE_ALL_POKEMON';
+export const RECEIVE_SINGLE_POKEMON = 'RECEIVE_SINGLE_POKEMON';
 
 export const receiveAllPokemon = pokemon => ({
   type: RECEIVE_ALL_POKEMON,
+  pokemon
+});
+
+export const receiveSinglePokemon = pokemon => ({
+  type: RECEIVE_SINGLE_POKEMON,
   pokemon
 });
 
@@ -12,3 +18,7 @@ export const requestAllPokemon = () => (dispatch) => (
     .then(pokemon => dispatch(receiveAllPokemon(pokemon)))
 );
 
+export const requestSinglePokemon = (id) => (dispatch) => (
+  PokemonAPIUtil.fetchSinglePokemon(id)
+    .then(pokemon => dispatch(requestSinglePokemon(pokemon)))
+);
