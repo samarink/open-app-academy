4cb73c5964abac71bbc880972367e4460f538a79
Konstantin Samarin <konstsamari@gmail.com>
2020-07-07 21:21:10 +0700

    pokedex p2: pokemon detail component

---
+++ 06_react/13-pokedex-p1/frontend/components/pokemon/pokemon_detail.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class PokemonDetail extends React.Component {
+  render() {
+    const { pokemon, items } = this.props;
+
+    return (
+      <section className="pokemon-detail">
+        <figure>
+          <img src={pokemon.image_url} alt={pokemon.name} />
+        </figure>
+        <ul>
+          <li>
+            <h2>{pokemon.name}</h2>
+          </li>
+          <li>Type: {pokemon.poke_type}</li>
+          <li>Attack: {pokemon.attack}</li>
+          <li>Defense: {pokemon.defense}</li>
+          <li>Moves: {pokemon.moves.join(', ')}</li>
+        </ul>
+        <section className="toys">
+          <h3>Items</h3>
+          <ul className="toy-list">
+            {items.map(item => <li key={item.id}>item.name</li>)}
+          </ul>
+        </section>
+      </section>
+    )
+  }
+}
+
+export default PokemonDetail;
+
