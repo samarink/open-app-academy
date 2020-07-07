2263611dcd3ea9be4fa2bf7c02143d09c82cda94
Konstantin Samarin <konstsamari@gmail.com>
2020-07-07 20:11:11 +0700

    pokedex p2: react root route

--- 06_react/13-pokedex-p1/frontend/pokedex.jsx
+++ 06_react/13-pokedex-p1/frontend/pokedex.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import { Provider } from 'react-redux';
+import { HashRouter, Route } from 'react-router-dom';
 import PokemonIndexContainer from './components/pokemon/pokemon_index_container';
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <PokemonIndexContainer />
+    <HashRouter>
+      <Route path='/' component={PokemonIndexContainer} />
+    </HashRouter>
   </Provider>
 );
 
