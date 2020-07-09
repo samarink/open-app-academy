74a7504d1cc12bccc388d3998b5d883702f8f6c2
Konstantin Samarin <konstsamari@gmail.com>
2020-07-09 23:26:41 +0700

    bnb: store

--- 06_react/16-bench-bnb/frontend/bench_bnb.jsx
+++ 06_react/16-bench-bnb/frontend/bench_bnb.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
+  // test
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
   const root = document.getElementById('root');
   ReactDOM.render(<h1>bnb</h1>, root);
 });
