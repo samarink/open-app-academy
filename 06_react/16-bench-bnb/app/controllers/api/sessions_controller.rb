965a79bb89fb10f67f41906ef344d60a24eb965e
Konstantin Samarin <konstsamari@gmail.com>
2020-07-09 18:07:20 +0700

    bnb: auth backend

---
+++ 06_react/16-bench-bnb/app/controllers/api/sessions_controller.rb
@@ -0,0 +1,26 @@
+class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user.nil?
+      render json: ['Credentials were wrong'], status: 401
+    else
+      login!(@user)
+      render 'api/users/show'
+    end
+  end
+
+  def destroy
+    @user = current_user
+
+    if @user
+      logout!
+      render json: {}
+    else
+      render json: ['Nobody signed in'], status: 404
+    end
+  end
+end
