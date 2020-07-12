1c3da74b1f0ed8148df442fa27163285f0d58af9
Konstantin Samarin <konstsamari@gmail.com>
2020-07-12 23:45:32 +0700

    mern twitter: routes

--- 07-mern/mern-twitter/app.js
+++ 07-mern/mern-twitter/app.js
@@ -2,13 +2,22 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+
+const users = require('./routes/api/users');
+const tweets = require('./routes/api/tweets');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB successfully'))
   .catch(err => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (_, res) => res.send('Hello World!'));
+app.use('/api/users', users);
+app.use('/api/tweets', tweets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on http://localhost:${port}`));
