0b3531ebaffd5b3ac7f1dfdc4d4e0e6820187a05
Konstantin Samarin <konstsamari@gmail.com>
2020-07-15 22:47:52 +0700

    mern twitter: tweets react/redux cycle

---
+++ 07-mern/mern-twitter/frontend/src/components/tweets/tweet_box.js
@@ -0,0 +1,11 @@
+class TweetBox extends React.Component {
+  render() {
+    return (
+        <div>
+            <h3>{this.props.text}</h3>
+        </div>
+    );
+  }
+}
+
+export default TweetBox;
