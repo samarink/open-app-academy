2788abc917d78d3f13f4899710b51843b822ff11
Konstantin Samarin <konstsamari@gmail.com>
2020-07-25 17:58:03 +0700

    gg: configure apollo

--- 09-graphql/03-greek-gods/client/index.js
+++ 09-graphql/03-greek-gods/client/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
+
+import { HashRouter } from 'react-router-dom';
+
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
+
+const cache = new InMemoryCache({
+  dataIdFromObject: object => object.id || null
+});
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: cache,
+  onError: ({ networkError, graphQLErrors }) => {
+    console.log('graphQLErrors', graphQLErrors);
+    console.log('networkError', networkError);
+  }
+});
 
 const Root = () => {
   return (
-    <div>
-      Root
-    </div>
+    <ApolloProvider client={client}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </ApolloProvider>
   );
 };
 
