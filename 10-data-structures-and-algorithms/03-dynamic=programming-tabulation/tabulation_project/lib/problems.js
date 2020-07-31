d4ad9400ce5aac10ff40692694bd4a09f1eaab25
Konstantin Samarin <konstsamari@gmail.com>
2020-07-31 15:19:07 +0700

    tabul project: complete

--- 10-data-structures-and-algorithms/03-dynamic=programming-tabulation/tabulation_project/lib/problems.js
+++ 10-data-structures-and-algorithms/03-dynamic=programming-tabulation/tabulation_project/lib/problems.js
@@ -20,10 +20,40 @@
 // stepper([3, 1, 0, 5, 10]);           // => true, because we can step through elements 3 -> 5 -> 10
 // stepper([3, 4, 1, 0, 10]);           // => true, because we can step through elements 3 -> 4 -> 10
 // stepper([2, 3, 1, 1, 0, 4, 7, 8])    // => false, there is no way to step to the end
-function stepper(nums) {
 
+// Tabulation
+// function stepper(nums) {
+//   let table = new Array(nums.length).fill(false);
+//   table[0] = true;
+
+//   for (let i = 0; i < nums.length; i++) {
+//     if (table[i]) {
+//       for (let j = 1; j <= nums[i]; j++) {
+//         table[i + j] = true;
+//       }
+//     }
+//   }
+
+//   return table[table.length - 1];
+// }
+
+// Memoization
+function stepper(nums, memo = {}) {
+  if (nums.length in memo) return memo[nums.length];
+
+  if (nums.length === 0) return true;
+  let maxRange = nums[0];
+
+  for (let step = 1; step <= maxRange; step++) {
+    if (stepper(nums.slice(step), memo)) {
+      memo[nums.length] = true;
+      return true;
+    }
+  }
+
+  memo[nums.length] = false;
+  return false;
 }
-
 
 // Write a function, maxNonAdjacentSum(nums), that takes in an array of nonnegative numbers.
 // The function should return the maximum sum of elements in the array we can get if we cannot take
@@ -35,10 +65,35 @@ function stepper(nums) {
 //
 // maxNonAdjacentSum([2, 7, 9, 3, 4])   // => 15, because 2 + 9 + 4
 // maxNonAdjacentSum([4,2,1,6])         // => 10, because 4 + 6
-function maxNonAdjacentSum(nums) {
 
+// function maxNonAdjacentSum(nums) {
+//   if (nums.length === 0) return 0;
+//   let table = new Array(nums.length).fill(0);
+//   table[0] = nums[0];
+
+//   for (let i = 1; i < table.length; i++) {
+//     let skipLeft = table[i - 2] === undefined ? 0 : table[i - 2];
+//     let includeNum = skipLeft + nums[i];
+//     let notIncludeNum = table[i - 1];
+//     table[i] = Math.max(includeNum, notIncludeNum);
+//   }
+
+//   return table[table.length - 1];
+// }
+
+function maxNonAdjacentSum(nums, memo = {}) {
+  if (nums.length in memo) return memo[nums.length];
+  if (nums.length === 0) return 0;
+
+  let firstEl = nums[0];
+
+  memo[nums.length] = Math.max(
+    firstEl + maxNonAdjacentSum(nums.slice(2), memo),
+    maxNonAdjacentSum(nums.slice(1), memo)
+  );
+
+  return memo[nums.length];
 }
-
 
 // Write a function, minChange(coins, amount), that accepts an array of coin values
 // and a target amount as arguments. The method should the minimum number of coins needed
@@ -53,12 +108,27 @@ function maxNonAdjacentSum(nums) {
 // minChange([1, 5, 10, 25], 15)    // => 2, because 10 + 5 = 15
 // minChange([1, 5, 10, 25], 100)   // => 4, because 25 + 25 + 25 + 25 = 100
 function minChange(coins, amount) {
+  let table = new Array(amount + 1).fill(Infinity);
+  table[0] = 0;
 
+  coins.forEach((coin) => {
+    for (let amt = 0; amt < table.length; amt++) {
+      for (let qty = 0; qty * coin <= amt; qty++) {
+        let remainder = amt - qty * coin;
+        let attempt = table[remainder] + qty;
+
+        if (attempt < table[amt]) {
+          table[amt] = attempt;
+        }
+      }
+    }
+  });
+
+  return table[table.length - 1];
 }
 
-
 module.exports = {
-    stepper,
-    maxNonAdjacentSum,
-    minChange
+  stepper,
+  maxNonAdjacentSum,
+  minChange,
 };
