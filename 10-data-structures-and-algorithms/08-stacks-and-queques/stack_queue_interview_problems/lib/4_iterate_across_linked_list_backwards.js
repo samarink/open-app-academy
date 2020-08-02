05d774277e415215ef80c16d09721dc1f7440027
Konstantin Samarin <konstsamari@gmail.com>
2020-08-02 17:46:13 +0700

    stacks and queques: interview problems complete

---
+++ 10-data-structures-and-algorithms/08-stacks-and-queques/stack_queue_interview_problems/lib/4_iterate_across_linked_list_backwards.js
@@ -0,0 +1,52 @@
+// ============================================================================
+// Interview Problem: Constant Time Stack Max
+// ============================================================================
+//
+// -------
+// Prompt:
+// -------
+//
+// Iterate over a Singly Linked List of primitives backwards. When finished,
+// return a string representing the original linked list's values backwards
+// in the following format:
+//
+//                             'A -> B -> C -> D'
+//
+// ------------
+// Constraints:
+// ------------
+//
+// (1) Your function must be iterative, not recursive.
+// (2) Your function must consume O(n) space.
+// (3) Employee either a Stack, Queue, or some combination of the two in your
+//     solution. (Implement any data structures you need, as you need them.)
+//
+//
+// -----------
+// Let's code!
+// -----------
+
+function iterateAcrossLinkedListBackwards(linkedList) {
+    // TODO: Implement the iterateAcrossLinkedListBackwards function here
+
+  const stack = [];
+  let result ='';
+  let current = linkedList.head;
+
+  while (current) {
+    stack.push(current);
+    current = current.next;
+  }
+
+  while (stack.length > 0) {
+    if (stack.length > 1) {
+      result += `${stack.pop().value} -> `
+    } else {
+      result += `${stack.pop().value}`
+    }
+  }
+
+  return result;
+}
+
+exports.iterateAcrossLinkedListBackwards = iterateAcrossLinkedListBackwards;
